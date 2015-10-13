1966a154ca9f2e7e8f0f0314254c3e649efad610
Sollace <mwchris@mweb.co.za>
2015-10-13 17:03:55 +0200

    Split up the file to better make sense

---
+++ js/actiontimer.js
@@ -0,0 +1,33 @@
+(function() {
+  setInterval(function() {
+    ha();
+  }, 1000);
+  
+  var timer = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  };
+  
+  $('.testBody span').click(function() {
+    timer[$(this).text('0').attr('class').replace('count ','')] = 0;
+  });
+  
+  function ha() {
+    if (++timer.seconds >= 60) {
+      timer.seconds = 0;
+      if (++timer.minutes >= 60) {
+        timer.minutes = 0;
+        ++timer.hours;
+        change('hours');
+      }
+      change('minutes');
+    }
+    change('seconds');
+  }
+
+  function change(name) {
+    $('.' + name).text(timer[name]);
+    $('.' + name + ',.' + name + ' + .letter').css('opacity', '1');
+  }
+})();
