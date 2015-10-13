bf515c265931a495e42857041a3e072c2c31d9fb
Sollace <mwchris@mweb.co.za>
2015-10-13 19:40:00 +0200

    Some more fun with the home page

--- js/actiontimer.js
+++ js/actiontimer.js
@@ -3,16 +3,30 @@
     ha();
   }, 1000);
   
+  
+  var colours = (function() {
+    var colours = [ '#ea80b0', '#6aaadd', '#e97135', '#5e51a3', '#e6b91f', '#a66ebe' ];
+    var pointer = 0;
+    return {
+      'next': function() {
+        pointer = (pointer + 1) % colours.length;
+        return colours[pointer];
+      }
+    }
+  })();
+  
+  var direction = 5;
+  var backgroundWidth = 0;
   var timer = {
     hours: 0,
     minutes: 0,
     seconds: 0
   };
-  
+
   $('.timer span.count').click(function() {
     timer[$(this).text('0').attr('class').replace('count ','')] = 0;
   });
-  
+
   function ha() {
     if (++timer.seconds >= 60) {
       timer.seconds = 0;
@@ -24,6 +38,16 @@
       change('minutes');
     }
     change('seconds');
+    bouncePinkie();
+  }
+
+  function bouncePinkie() {
+    backgroundWidth += direction;
+    if (backgroundWidth >= 100 && direction > 0) {
+      direction = -direction;
+      $('.testBody').css('background-color', colours.next());
+    }
+    if (backgroundWidth <= 0 && direction < 0) direction = -direction;
   }
 
   function change(name) {
